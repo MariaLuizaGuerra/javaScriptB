08faeeb8d2c346f5058df415b64e6ce62fb9cee7
MariaLuizaGuerra <marialuizaguerrapedroso@gmail.com>
2025-07-01 12:30:39 -0300

    aula 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,19 @@
+var global = "variavel global";
+console.log(global);
+function exeomplo2(){
+    var local="variavel local";
+    console.log(local);
+}
+exeomplo2();
+
+let x=10;
+if(x>5){
+    var y=18;
+    console.log("valor de y igual a",y);
+}
+const constante ="texto";
+console.log(constante);
+
+function saudacao(){
+    alert("Bem vindo ao nosso site 3");
+}
