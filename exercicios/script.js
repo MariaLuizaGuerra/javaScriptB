e525c8ed872738fd15bca59bd723678d4ed9e528
MariaLuizaGuerra <marialuizaguerrapedroso@gmail.com>
2025-07-15 12:35:30 -0300

    15/07

--- exercicios/script.js
+++ exercicios/script.js
@@ -4,6 +4,7 @@ function somar(){
     let n2 = Number(document.getElementById("num2").value);
     document.getElementById("resultado1").innerText = "Resultado: " + (n1 + n2);
 }
+//2
 function idade(){
     let idade = Number(document.getElementById("idade").value);
     if(idade >=18){
@@ -11,6 +12,27 @@ function idade(){
     }
     else{
         document.getElementById("maiorIdade").innerText = "Menor de idade";
+    } 
+}
+//3
+function contarCaracteres() {
+    let texto = (document.getElementById("texto").value);
+    let resultado = (document.getElementById("resultado3"));
+    resultado.textContent = "Total de caracteres: " + texto.length;
+}
+//4
+function verificar(){
+    let numero = Number(document.getElementById("numero").value);
+    if(numero % 2 == 0){
+        document.getElementById("resultado4").innerText = "Par";
     }
-    
+    else{
+        document.getElementById("resultado4").innerText = "Impar";
+    } 
+}
+//5
+function caixaAuta() {
+    let texto = (document.getElementById("texto5").value);
+    let resultado = (document.getElementById("resultado5"));
+    resultado.textContent = "Caixa alta: " + texto.toUpperCase();
 }
